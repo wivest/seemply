f8e24141c323e7eafd2c38165cdbfcddd42a6845
wivest <120594169+wivest@users.noreply.github.com>
2025-01-15 22:44:00 +0100

    feat: check if file exists

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
+use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let filename = &args[1];
+
+    let exists = Path::new(filename).is_file();
+    dbg!(exists);
 }
