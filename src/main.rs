c316af783caa765ef1272f7f9d3d629dc160d604
wivest <120594169+wivest@users.noreply.github.com>
2025-01-15 22:47:10 +0100

    feat: check number of arguments

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::path::Path;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
+    if args.len() != 2 {
+        panic!();
+    }
 
     let exists = Path::new(filename).is_file();
     dbg!(exists);
