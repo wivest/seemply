3d5ce60216d915237468040ac743887b0abe34ea
wivest <120594169+wivest@users.noreply.github.com>
2025-01-18 23:39:05 +0100

    feat: print file content

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::env;
+use std::fs;
 use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = validate(&args);
 
-    let exists = Path::new(filename).is_file();
-    dbg!(exists);
+    let content = get_content(filename);
+    println!("{}", content);
 }
 
 fn validate(args: &Vec<String>) -> &String {
@@ -14,4 +15,12 @@ fn validate(args: &Vec<String>) -> &String {
         panic!();
     }
     return &args[1];
+}
+
+fn get_content(filename: &String) -> String {
+    let exists = Path::new(&filename).is_file();
+    if !exists {
+        panic!();
+    }
+    return fs::read_to_string(filename).unwrap();
 }
