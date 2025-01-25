eb53b3f5fedf176dcd9d9a50816a0db556f85b2d
wivest <120594169+wivest@users.noreply.github.com>
2025-01-25 23:29:17 +0100

    refactor: extract functions into module

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
+mod console;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = validate(&args);
     let content = get_content(filename);
-    init();
+    console::init();
 
     println!("{}", content);
     let mut buffer: String = String::new();
     std::io::stdin()
         .read_line(&mut buffer)
         .expect("Input error!");
-    drop();
+    console::drop();
 }
 
 fn validate(args: &Vec<String>) -> &String {
@@ -29,13 +31,4 @@ fn get_content(filename: &String) -> String {
         panic!("File at specified path doesn't exist!");
     }
     return fs::read_to_string(filename).unwrap();
-}
-
-fn init() {
-    print!("\x1B[?1049h");
-    print!("\x1B[H");
-}
-
-fn drop() {
-    print!("\x1B[?1049l");
 }
