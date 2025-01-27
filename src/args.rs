dd40782a53dfda08021c958cc6e07bbb693797ad
wivest <120594169+wivest@users.noreply.github.com>
2025-01-27 23:04:49 +0100

    refactor: remove cloning

--- src/args.rs
+++ src/args.rs
@@ -2,12 +2,12 @@ use std::env;
 
 pub fn get_filename() -> String {
     let args: Vec<String> = env::args().collect();
-    validate(&args).clone()
+    validate(args).pop().expect("Validation failed!")
 }
 
-fn validate(args: &Vec<String>) -> &String {
+fn validate(args: Vec<String>) -> Vec<String> {
     if args.len() != 2 {
         panic!("Argument length must be 2!");
     }
-    return &args[1];
+    return args;
 }
