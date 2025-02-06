784fc970120175c3116449c67c9a7b27bbeeeb3e
wivest <120594169+wivest@users.noreply.github.com>
2025-02-06 14:14:53 +0100

    feat: edit command

--- src/console/platform.rs
+++ src/console/platform.rs
@@ -3,12 +3,15 @@ use std::process::Command;
 #[cfg(target_os = "windows")]
 pub fn get_command(sequence: &str) -> Command {
     let mut command = Command::new("cmd");
-    command.args(["/C", "echo", sequence]);
+    command.args([
+        "/c", "echo", sequence, "|", "set", "/p", "size=", "&", "echo", "%size%",
+    ]);
     command
 }
 
 #[cfg(not(target_os = "windows"))]
 pub fn get_command(sequence: &str) -> Command {
+    // TODO: not implemented!
     let mut command = Command::new("sh");
     command.args(["-c", "echo", sequence]);
     command
