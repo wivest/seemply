2c641374b8b1238a44ac24a6954be174c3170c00
wivest <120594169+wivest@users.noreply.github.com>
2025-02-06 14:12:59 +0100

    refactor: use attributes

--- src/console/platform.rs
+++ src/console/platform.rs
@@ -1,13 +1,15 @@
 use std::process::Command;
 
+#[cfg(target_os = "windows")]
 pub fn get_command(sequence: &str) -> Command {
-    let mut command: Command;
-    if cfg!(target_os = "windows") {
-        command = Command::new("cmd");
-        command.args(["/C", "echo", sequence]);
-    } else {
-        command = Command::new("sh");
-        command.args(["-c", "echo", sequence]);
-    }
-    return command;
+    let mut command = Command::new("cmd");
+    command.args(["/C", "echo", sequence]);
+    command
+}
+
+#[cfg(not(target_os = "windows"))]
+pub fn get_command(sequence: &str) -> Command {
+    let mut command = Command::new("sh");
+    command.args(["-c", "echo", sequence]);
+    command
 }
