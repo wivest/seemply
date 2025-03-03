b9d0a1087561a7eb5dfdc1fbbfb6183da82825c1
wivest <120594169+wivest@users.noreply.github.com>
2025-03-03 05:36:04 +0100

    fix: dismiss key releases

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use console::{Console, State};
-use crossterm::event::{Event, KeyCode};
+use crossterm::event::{Event, KeyCode, KeyEventKind};
 use std::fs;
 
 mod args;
@@ -15,37 +15,39 @@ fn main() {
     loop {
         let event = Console::ask_command().expect("Failed to ask command!");
         if let Event::Key(key) = event {
-            if key.code == KeyCode::Char('q') {
-                break;
-            }
-            if key.code == KeyCode::Char('w') {
-                let delta = con.cursor.up(1);
-                if delta != 0 {
-                    con.scroll_up(delta);
+            if key.kind != KeyEventKind::Release {
+                if key.code == KeyCode::Char('q') {
+                    break;
                 }
-                let line = con.get_line_width();
-                con.cursor.right(0, line);
-            }
-            if key.code == KeyCode::Char('s') {
-                let delta = con.cursor.down(1);
-                if delta != 0 {
-                    con.scroll_down(delta);
+                if key.code == KeyCode::Char('w') {
+                    let delta = con.cursor.up(1);
+                    if delta != 0 {
+                        con.scroll_up(delta);
+                    }
+                    let line = con.get_line_width();
+                    con.cursor.right(0, line);
                 }
-                let line = con.get_line_width();
-                con.cursor.right(0, line);
-            }
-            if key.code == KeyCode::Char('a') {
-                con.cursor.left(1);
-            }
-            if key.code == KeyCode::Char('d') {
-                let line = con.get_line_width();
-                con.cursor.right(1, line);
-            }
-            if key.code == KeyCode::Char('i') {
-                con.state = State::Input;
-            }
-            if key.code == KeyCode::Esc {
-                con.state = State::Control;
+                if key.code == KeyCode::Char('s') {
+                    let delta = con.cursor.down(1);
+                    if delta != 0 {
+                        con.scroll_down(delta);
+                    }
+                    let line = con.get_line_width();
+                    con.cursor.right(0, line);
+                }
+                if key.code == KeyCode::Char('a') {
+                    con.cursor.left(1);
+                }
+                if key.code == KeyCode::Char('d') {
+                    let line = con.get_line_width();
+                    con.cursor.right(1, line);
+                }
+                if key.code == KeyCode::Char('i') {
+                    con.state = State::Input;
+                }
+                if key.code == KeyCode::Esc {
+                    con.state = State::Control;
+                }
             }
         }
         con.print().expect("Failed to print content!");
