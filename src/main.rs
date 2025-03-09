c37bd65e5cbc65f194554ebdf7b6fdb9f69634fc
wivest <120594169+wivest@users.noreply.github.com>
2025-03-09 23:56:39 +0100

    refactor: use if let statement

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,20 @@ mod content;
 fn main() {
     let path = args::get_filename();
 
-    let mut con = Console::new(&path).expect("Failed to initialize console!");
-    con.print().expect("Failed to print content!");
-
-    loop {
-        let event = Console::ask_command().expect("Failed to read command!");
-        if let Event::Key(key) = event {
-            if key.kind != KeyEventKind::Release {
-                if !con.state.handle_input(key.code, &mut con) {
-                    break;
+    if let Ok(mut con) = Console::new(&path) {
+        con.print().expect("Failed to print content!");
+        loop {
+            let event = Console::ask_command().expect("Failed to read command!");
+            if let Event::Key(key) = event {
+                if key.kind != KeyEventKind::Release {
+                    if !con.state.handle_input(key.code, &mut con) {
+                        break;
+                    }
                 }
             }
+            con.print().expect("Failed to print content!");
         }
-        con.print().expect("Failed to print content!");
+    } else {
+        panic!("Failed to initialize console!");
     }
 }
